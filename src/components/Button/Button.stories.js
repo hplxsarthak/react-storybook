227d34b869b39175f000710bddbfd20858bc2a66
Sarthak <sarthak.agrawal@healthplix.com>
2022-07-12 10:11:21 +0530

    Using args

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,6 +4,9 @@ import Button from "./Button";
 export default {
     title: 'Form/Button', // This is mandatory and it's unique in whole project
     component: Button,
+    args: {              //  Defining args in the default
+        children: "Button"
+    }
 }
 
 // Every named exports define a story
@@ -15,3 +18,24 @@ export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+// Args approach to write stories
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+    variant: 'primary',
+    children: 'Primary Args'
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+    variant: 'secondary',
+    // children: 'Secondary Args'
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+    ...PrimaryA.args,
+    children: 'Long Primary Args'
+}
